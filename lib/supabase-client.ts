84607f3108a7eda5312505dce30b74c1e9bfc213
0xsj <sjtommylee@gmail.com>
2025-02-11 23:55:32 -0800

    updated storage interface

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -1,16 +1,34 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
+import { MMKV } from "react-native-mmkv";
 
 const supabaseUrl = process.env.EXPO_SUPABASE_URL;
-const supbaseAnonKey = process.env.EXPO_SUPABASE_ANON_KEY;
+const supabaseAnonKey = process.env.EXPO_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supbaseAnonKey) {
+const storage = new MMKV();
+
+const MMKVStorage = {
+  setItem: (key: string, value: string) => {
+    storage.set(key, value);
+    return Promise.resolve();
+  },
+  getItem: (key: string) =>{
+    const value = storage.getString(key);
+    return Promise.resolve(value ?? null);
+  },
+  removeItem: (key: string) => {
+    storage.delete(key);
+    return Promise.resolve();
+  }
+}
+
+
+if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Supabase URL and anon key must be defined in .env");
 }
 
-export const supabase = createClient(supabaseUrl, supbaseAnonKey, {
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: AsyncStorage,
+    storage: MMKVStorage,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
